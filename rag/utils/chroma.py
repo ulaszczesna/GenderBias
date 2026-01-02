a99d8a6bc795fcfd99a945831fd6eb07fe121be8
martyna <01171248@pw.edu.pl>
2026-01-02 13:14:30 +0100

    add RAG folder from rag branch

---
+++ rag/utils/chroma.py
@@ -0,0 +1,17 @@
+import chromadb
+
+class ChromaDatabase:
+    def __init__(self, collection_name: str = "gender_equality_docs", path: str = "../chroma_db"):
+        self.client = chromadb.PersistentClient(path)
+        self.collection = self.client.get_or_create_collection(name=collection_name)
+
+
+    def add_embeddings(self, texts, embeddings, prefix,  metadatas= None):
+        ids = [f"{prefix}_{i}" for i in range(len(texts))]
+        if metadatas is None:
+            metadatas = [{}] * len(texts)
+        self.collection.add(documents=texts, embeddings=embeddings, metadatas=metadatas, ids=ids)
+
+    def query(self, query_embedding, n_results=10):
+        results = self.collection.query(query_embeddings=[query_embedding], n_results=n_results)
+        return results["documents"][0]
